239b9d156cb1d44c44e97de2d119896398a9c549
Aditya Mishra <adityamishra200111@gmail.com>
2023-09-29 17:56:47 +0530

    font color green

---
+++ newone/dom.js
@@ -0,0 +1,10 @@
+var headerTitle = document.getElementById("header-title");
+var headerTitle = document.getElementById("main-header");
+
+var titlemew = document.getElementsByClassName("title");
+titlemew[0].style.fontWeight = "bold";
+titlemew[0].style.color = "green";
+
+console.log(titlemew);
+
+headerTitle.style.borderBottom = "solid 3px #000";
